5330d7adfa0299b2332ddab2c89bac31cd5f06e4
Pradana97 <pradana.yanuar77@gmail.com>
2020-09-02 09:27:04 +0700

    revisi tampilan rekening

--- views/rekening/_form.php
+++ views/rekening/_form.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
+use kartik\file\FileInput;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\MRekening */
@@ -13,37 +14,82 @@ use yii\helpers\ArrayHelper;
 <div class="mrekening-form">
 
     <?php $form = ActiveForm::begin(); ?>
+    <div class="row">
 
-    <?= $form->field($model, 'id_data')->widget(Select2::classname(), [
-        'data' => ArrayHelper::map(\app\models\MBiodata::find()->where(['is_pegawai'=>1])->all(), 'id_data', 'nama'),
-        'options' => ['placeholder' => 'Select id_data ...'],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ])->label('Nama Pegawai')
-    ?>
+        <div class="col-xs-4">
+            <?= $form->field($model, 'id_data')->widget(Select2::classname(), [
+                'data' => ArrayHelper::map(\app\models\MBiodata::find()->where(['is_pegawai' => 1])->all(), 'id_data', 'nama'),
+                'options' => ['placeholder' => 'Select id_data ...'],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ])->label('Nama Pegawai')
+            ?>
 
-    <?= $form->field($model, 'bank_id')->widget(Select2::classname(), [
-        'data' => ArrayHelper::map(\app\models\MReferensi::find()->where(['tipe_referensi' => '5'])->all(), 'reff_id', 'nama_referensi'),
-        'options' => ['placeholder' => 'Select bank id ...'],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ])->label('Bank')
-    ?>
+            <?= $form->field($model, 'bank_id')->widget(Select2::classname(), [
+                'data' => ArrayHelper::map(\app\models\MReferensi::find()->where(['tipe_referensi' => '5'])->all(), 'reff_id', 'nama_referensi'),
+                'options' => ['placeholder' => 'Select bank id ...'],
+                'pluginOptions' => [
+                    'allowClear' => true
+                ],
+            ])->label('Bank')
+            ?>
 
-    <?= $form->field($model, 'nomor_rekening')->textInput(['maxlength' => true,'type'=>'number']) ?>
+            <?= $form->field($model, 'nomor_rekening')->textInput(['maxlength' => true, 'type' => 'number']) ?>
 
-    <?= $form->field($model, 'npwp')->textInput(['maxlength' => true,'type'=>'number']) ?>
+            <?= $form->field($model, 'npwp')->textInput(['maxlength' => true, 'type' => 'number']) ?>
+        </div>
 
-    <?= $form->field($model, 'fotoNpwp')->fileInput() ?>
+        <div class="col-xs-4">
+            <?php
+            if (!$model->isNewRecord) {
+                $linkfotnpwp = \Yii::getAlias('@web/uploads/foto/' . $model->data->nip . '/' . $model->fotoNpwp);
+                if (file_exists(\Yii::getAlias('@uploads') . $model->data->nip . '/' . $model->fotoNpwp) && !empty($model->fotoNpwp)) {
+                    echo Html::a(Html::img($linkfotnpwp, ['class' => 'col-xs-12']), $linkfotnpwp);
+                }
+            }
+            ?>
+            <?= $form->field($model, 'fotoNpwp')->widget(FileInput::classname(), [
+                'options' => ['accept' => 'image/*', 'application/pdf'],
+                'pluginOptions' => [
+                    'maxFileSize' => 2048,
+                    'showPreview' => $model->isNewRecord,
+                    'showCaption' => false,
+                    'showRemove' => false,
+                    'showUpload' => false,
+                    'browseClass' => 'btn btn-primary btn-block',
+                    'browseIcon' => '<i class="glyphicon glyphicon-camera"></i> ',
+                    'browseLabel' =>  'Select Foto'
+                ],
+            ]) ?>
+        </div>
 
-    <?= $form->field($model, 'fotoRekening')->fileInput() ?>
+        <div class="col-xs-4">
+            <?php
+            if (!$model->isNewRecord) {
+                $linkfotrek = \Yii::getAlias('@web/uploads/foto/' . $model->data->nip . '/' . $model->fotoRekening);
+                if (file_exists(\Yii::getAlias('@uploads') . $model->data->nip . '/' . $model->fotoRekening) && !empty($model->fotoRekening)) {
+                    echo Html::a(Html::img($linkfotrek, ['class' => 'col-xs-12']), $linkfotrek);
+                }
+            }
+            ?>
+            <?= $form->field($model, 'fotoRekening')->widget(FileInput::classname(), [
+                'options' => ['accept' => 'image/*', 'application/pdf'],
+                'pluginOptions' => [
+                    'maxFileSize' => 2048,
+                    'showPreview' => $model->isNewRecord,
+                    'showCaption' => false,
+                    'showRemove' => false,
+                    'showUpload' => false,
+                    'browseClass' => 'btn btn-primary btn-block',
+                    'browseIcon' => '<i class="glyphicon glyphicon-camera"></i> ',
+                    'browseLabel' =>  'Select Foto'
+                ],
+            ]) ?>
+            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        </div>
 
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
-
     <?php ActiveForm::end(); ?>
 
 </div>
